2d84b8c9f8815a23a517fc4074d678455299a20e
Hugo <hugo.ct93@gmail.com>
2019-07-25 18:28:01 -0300

    .

--- android/src/main/java/io/github/hugoct/HugoAudioRecorder/HugoAudioRecorderManager.kt
+++ android/src/main/java/io/github/hugoct/HugoAudioRecorder/HugoAudioRecorderManager.kt
@@ -1,6 +1,8 @@
 package io.github.hugoct.HugoAudioRecorder
 
 import android.widget.Toast
+import android.media.MediaRecorder
+import java.io.IOException
 
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
@@ -9,19 +11,43 @@ import com.facebook.react.bridge.ReactMethod
 
 class HugoAudioRecorderManager(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
 
+    var recorder: MediaRecorder? = null
+
     override fun getName(): String {
         return "HugoAudioRecorder"
     }
 
     @ReactMethod
     fun startRecord(fileName: String) {
-        Toast.makeText(getReactApplicationContext(), "Start", Toast.LENGTH_LONG).show()
+        Toast.makeText(getReactApplicationContext(), "Gravando...", Toast.LENGTH_LONG).show()
+        
+
+        recorder = MediaRecorder().apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+            setOutputFile("${externalCacheDir.absolutePath}/audiorecordtest.3gp")
+            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+
+            try {
+                prepare()
+            } catch (e: IOException) {
+                Log.e(LOG_TAG, "prepare() failed")
+                Toast.makeText(getReactApplicationContext(), "Erro!", Toast.LENGTH_LONG).show()
+            }
+
+            start()
+        }
     }
 
     @ReactMethod
     fun stopRecord() {
         Toast.makeText(getReactApplicationContext(), "Stop", Toast.LENGTH_LONG).show()
         
+        recorder?.apply {
+            stop()
+            release()
+        }
+        recorder = null
     }
 
 }
