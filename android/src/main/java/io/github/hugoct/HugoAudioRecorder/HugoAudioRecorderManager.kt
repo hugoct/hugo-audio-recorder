c572d30e00f3771fca889cf944aa9efeeed9f228
Hugo <hugo.ct93@gmail.com>
2019-07-26 10:24:52 -0300

    .

--- android/src/main/java/io/github/hugoct/HugoAudioRecorder/HugoAudioRecorderManager.kt
+++ android/src/main/java/io/github/hugoct/HugoAudioRecorder/HugoAudioRecorderManager.kt
@@ -2,52 +2,103 @@ package io.github.hugoct.HugoAudioRecorder
 
 import android.widget.Toast
 import android.media.MediaRecorder
+import android.media.MediaPlayer
+import java.io.File
 import java.io.IOException
+import android.os.Environment
 
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.bridge.Callback
 
 
 class HugoAudioRecorderManager(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
 
-    var recorder: MediaRecorder? = null
+    private var recorder: MediaRecorder? = null
+    private var player: MediaPlayer? = null
+    var path = "${Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC)}/audiorecordtest.3gp"
 
     override fun getName(): String {
         return "HugoAudioRecorder"
     }
 
     @ReactMethod
-    fun startRecord(fileName: String) {
-        Toast.makeText(getReactApplicationContext(), "Gravando...", Toast.LENGTH_LONG).show()
+    fun startRecord(fileName: String, successCallback: Callback, errorCallback: Callback) {
         
-
-        recorder = MediaRecorder().apply {
-            setAudioSource(MediaRecorder.AudioSource.MIC)
-            setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
-            setOutputFile("${externalCacheDir.absolutePath}/audiorecordtest.3gp")
-            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
-
-            try {
+        Toast.makeText(getReactApplicationContext(), path, Toast.LENGTH_LONG).show()
+        
+        try {
+            recorder = MediaRecorder().apply {
+                setAudioSource(MediaRecorder.AudioSource.MIC)
+                setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+                setOutputFile(path)
+                setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
                 prepare()
-            } catch (e: IOException) {
-                Log.e(LOG_TAG, "prepare() failed")
-                Toast.makeText(getReactApplicationContext(), "Erro!", Toast.LENGTH_LONG).show()
+                start()
             }
-
-            start()
+            successCallback.invoke(true)
+        
+        } catch (e: IOException) {
+            errorCallback.invoke(false)
         }
     }
 
     @ReactMethod
-    fun stopRecord() {
+    fun stopRecord(successCallback: Callback, errorCallback: Callback) {
         Toast.makeText(getReactApplicationContext(), "Stop", Toast.LENGTH_LONG).show()
         
-        recorder?.apply {
-            stop()
-            release()
+        try {
+            recorder?.apply {
+                stop()
+                release()
+            }
+            recorder = null
+            successCallback.invoke(true);
+        } catch (e: IOException) {
+            errorCallback.invoke(false);
         }
-        recorder = null
+    }
+
+    @ReactMethod
+    fun playRecord(filename: String) {
+        Toast.makeText(getReactApplicationContext(), "Plaing...", Toast.LENGTH_LONG).show()
+        
+        player = MediaPlayer().apply {
+            try {
+                setDataSource(path)
+                prepare()
+                start()
+            } catch (e: IOException) {
+                Toast.makeText(getReactApplicationContext(), "Erro!", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    @ReactMethod
+    fun stopPlayingRecord() {
+        Toast.makeText(getReactApplicationContext(), "Stop Plaing", Toast.LENGTH_LONG).show()
+        
+        player?.release()
+        player = null
+    }
+
+    @ReactMethod
+    fun deleteRecord(fileName: String, successCallback: Callback, errorCallback: Callback) {
+        try {
+        var file = File(path)
+        
+        if (file.exists()) {
+            file.delete()
+        }
+
+        Toast.makeText(getReactApplicationContext(), "Deleted!", Toast.LENGTH_LONG).show()
+        successCallback.invoke(true);
+
+        } catch (e: IOException) {
+            errorCallback.invoke(false);
+        }
+        
     }
 
 }
--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 import { NativeModules } from "react-native";
 
-export const startRecord = (fileName) => NativeModules.HugoAudioRecorder.startRecord(fileName);
-export const stopRecord = () => NativeModules.HugoAudioRecorder.stopRecord();
+export const startRecord = (fileName, successCallback, errorCallback) =>
+  NativeModules.HugoAudioRecorder.startRecord(
+    fileName,
+    successCallback,
+    errorCallback
+  );
+
+export const stopRecord = (successCallback, errorCallback) =>
+  NativeModules.HugoAudioRecorder.stopRecord(successCallback, errorCallback);
+
+export const playRecord = fileName =>
+  NativeModules.HugoAudioRecorder.playRecord(fileName);
+
+export const stopPlayingRecord = () =>
+  NativeModules.HugoAudioRecorder.stopPlayingRecord();
+
+export const deleteRecord = (fileName, successCallback, errorCallback) =>
+  NativeModules.HugoAudioRecorder.deleteRecord(
+    fileName,
+    successCallback,
+    errorCallback
+  );
